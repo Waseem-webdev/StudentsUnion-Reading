002e80c17c8175658244fa34c8f46199ea035301
Waseem Ahmad <waseem.webdev@gmail.com>
2017-06-11 01:04:32 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>RUSU Task</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    <!-- Editable CSS -->
+     <link href="css/custom.css" rel="stylesheet">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   	
+</head>
+
+<body>
+
+      <!-- Navigation-Account: BEGIN -->
+    <?php include('nav.html');?>
+    <!-- Navigation-Account: END -->
+
+    <!-- Page Content -->
+    <div class="container">
+
+<!-- Top Banner: BEGGINING-->
+			<?php include('banner-slider.html');?>	
+			<!-- Top Banner: END-->
+
+
+        <!-- Content Row -->
+        <div class="row">
+            <div class="col-md-8">
+                    <figure>
+                <img class="img-responsive" src="img/MK004_All-RUSU-student-officers.jpg" alt="student officers">
+           
+             <figcaption>YOUR STUDENT OFFICERS 2017/18</figcaption>
+             </figure>
+            <div class="divider-panel"> </div>
+            </div>
+            
+            <!-- /.col-md-8 -->
+            <div class="col-md-4">
+    <a href="#">
+             <img class="img-responsive" src="img/ci2.jpg" alt="change img">
+              </a>
+            </div>
+            <!-- /.col-md-4 -->
+        </div>
+        <!-- /.row -->
+
+    
+
+        <!-- Content Row -->
+<div class="row">
+<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+<a class="thumbnail" href="#">
+    <img class="img-responsive" src="img/ACADEMIC-REPS-LOGO-01-3-edit.jpg" alt="academic reps">
+    </a>
+            </div>
+    <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                <img class="img-responsive" src="img/Flirt2.jpg" alt="flirt">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="img/housing2.jpg" alt="housing">
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                <a class="thumbnail" href="#">
+                    <img class="img-responsive" src="img/Nus-website-link2.jpg" alt="NUS">
+                </a>
+            </div>
+         </div>
+            <!-- /.col-lg-3 -->
+      
+        <!-- /.row -->
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                <p><small>Copyright &copy; Your Website 2017</small></p>
+                </div>
+            </div>
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+
+
+</body>
+
+</html>
